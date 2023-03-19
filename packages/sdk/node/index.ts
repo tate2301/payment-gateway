fa71638cc33b036062a97ede86d475104f676e55
Kamfeskaya <tatendachris@gmail.com>
2023-03-19 21:39:19 +0200

    feat: sample of sdk

--- packages/sdk/node/index.ts
+++ packages/sdk/node/index.ts
@@ -1,1 +1,33 @@
-export {};
+interface Payments {
+  create: () => void;
+  authorize: () => void;
+}
+
+class GatewayPayments implements Payments {
+  api_key: string;
+
+  create = () => null;
+  authorize = () => null;
+
+  constructor(api_key: string) {
+    this.api_key = api_key;
+  }
+}
+
+export default class Gateway {
+  api_key: string;
+  payments!: Payments;
+
+  constructor(api_key: string) {
+    this.api_key = api_key;
+    this.init();
+  }
+
+  init = () => {
+    this.payments = new GatewayPayments("");
+  };
+}
+
+const gateway = new Gateway("");
+
+gateway.payments.create();
